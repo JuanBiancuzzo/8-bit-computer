b087ba619901de975782c1a5e4044a19c8e8e22a
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2024-12-06 19:26:43 -0300

    Haciendo hasta obtener final de parejas

--- mapa-algo.py
+++ mapa-algo.py
@@ -2,6 +2,15 @@ import numpy as np
 
 EPSILON = 0.0001
 
+class Numero:
+
+    def __init__(self, numeros):
+        self.numeros = numeros
+        self.usado = False
+
+    def fueUsado(self):
+        self.usado = True
+
 def numBitUno(num):
     bitUnos = 0
     while num != 0:
@@ -9,6 +18,23 @@ def numBitUno(num):
             bitUnos += 1
         num //= 2
     return bitUnos
+
+def diferenciaNum(numeros):
+    assert(len(numeros) > 0)
+
+    if len(numeros) == 1:
+        return (numeros[0], 0)
+
+    primero, diferenciaPrimero = diferenciaNum(numeros[:len(numeros)//2])
+    segundo, diferenciaSegundo = diferenciaNum(numeros[len(numeros)//2:])
+
+    assert(diferenciaPrimero == diferenciaSegundo)
+
+    diferencia = primero ^ segundo
+    mascara = ~diferencia
+    assert(primero & mascara == segundo & mascara)
+
+    return (primero & mascara, diferenciaPrimero + diferencia)
 
 def reducirDiferencia(numeros):
     if len(numeros) <= 2:
@@ -29,12 +55,12 @@ def diferenciaUnBit(numeros: list[int]):
         numSiguiente = reducirDiferencia(numeros[len(numeros)//2:])
 
     diferencia = numSiguiente ^ numPrevio
-    log2diferencia = np.log2(diferencia)
+    log2diferencia = np.log2(diferencia) 
     fraccion = log2diferencia - int(log2diferencia)
 
     return -EPSILON < fraccion and fraccion < EPSILON
 
-def quineMcCluskey(bit, f):
+def quineMcCluskey(bit: int, f) -> list:
     """
         bit: cantidad de bits que se quiere usar
         f: una funcion que devuelve true si la funcion 
@@ -48,7 +74,7 @@ def quineMcCluskey(bit, f):
         if not f(num):
             continue
 
-        numeros[numBitUno(num)].append(num)
+        numeros[numBitUno(num)].append(Numero([num]))
     
     parejas = []
     for i in range(len(numeros) - 1):
@@ -58,14 +84,66 @@ def quineMcCluskey(bit, f):
 
         for numPrevio in previo:
             for numSiguiente in siguiente:
-                if not diferenciaUnBit([numPrevio, numSiguiente]):
+                if diferenciaNum([*numPrevio.numeros])[1] != diferenciaNum([*numSiguiente.numeros])[1]:
+                    continue
+
+                if not diferenciaUnBit([*numPrevio.numeros, *numSiguiente.numeros]):
                     continue
                     
-                parejas[i].append((numPrevio, numSiguiente))
+                numPrevio.fueUsado()
+                numSiguiente.fueUsado()
+                parejas[i].append(Numero([*numPrevio.numeros, *numSiguiente.numeros]))
 
-    return parejas
+    parejasNoUsadas = []
+    for nivel in numeros:
+        parejasNoUsadas += list(filter(lambda numero: not numero.usado, nivel))
+
+    parejas2 = []
+    for i in range(len(parejas) - 1):
+        parejas2.append([])
+        previo = parejas[i]
+        siguiente = parejas[i + 1]
+
+        if len(previo) == 0 or len(siguiente) == 0:
+            continue
+
+        for numPrevio in previo:
+            for numSiguiente in siguiente:
+                if diferenciaNum([*numPrevio.numeros])[1] != diferenciaNum([*numSiguiente.numeros])[1]:
+                    continue
+
+                if not diferenciaUnBit([*numPrevio.numeros, *numSiguiente.numeros]):
+                    continue
+                    
+                numPrevio.fueUsado()
+                numSiguiente.fueUsado()
+                parejas2[i].append(Numero([*numPrevio.numeros, *numSiguiente.numeros]))
+
+
+    for nivel in parejas:
+        parejasNoUsadas += list(filter(lambda numero: not numero.usado, nivel))
+
+    for nivel in parejas2:
+        parejasNoUsadas += nivel
+
+    parejasFinales = {}
+    for pareja in parejasNoUsadas:
+        diferencia = diferenciaNum([*pareja.numeros])[1]
+        if not diferencia in parejasFinales:
+            parejasFinales[diferencia] = pareja
+
+    return parejasFinales.values()
     
 #                              0,    1,    2,                   5,   6,    7,    8,     9,   10,                        14
 funcionInicial = lambda num: [True, True, True, False, False, True, True, True, True, True, True, False, False, False, True, False][num]
 
-print(quineMcCluskey(4, funcionInicial))
+resultado = quineMcCluskey(4, funcionInicial)
+for pareja in resultado:
+    print(f"{pareja.numeros} - {pareja.usado}")
+
+"""
+for i, nivel in enumerate(resultado):
+    print(f"Nivel {i}")
+    for pareja in nivel:
+        print(f"{pareja.numeros} - {pareja.usado}")
+"""
