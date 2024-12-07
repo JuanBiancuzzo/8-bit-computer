fd62d14653a1b8792542f1c1cc8e3d3b7197c282
Juan Biancuzzo <jbiancuzzo@fi.uba.ar>
2024-12-06 21:04:18 -0300

    Terminando algoritmo

--- mapa-algo.py
+++ mapa-algo.py
@@ -60,11 +60,50 @@ def diferenciaUnBit(numeros: list[int]):
 
     return -EPSILON < fraccion and fraccion < EPSILON
 
-def quineMcCluskey(bit: int, f) -> list:
+def obtenerRepresentacionBinaria(pareja, numeros):
+    representacion = 0
+    for numero in pareja.numeros:
+        try:
+            representacion += 1 << numeros.index(numero)
+        except:
+            pass
+
+    return representacion
+
+def obtenerTerminosEscenciales(representaciones):
+    escenciales = []
+    primos = []
+    for i, (pareja, representacion) in enumerate(representaciones):
+        esEscencial = representacion
+
+        for j, (_, representacionOpuesta) in enumerate(representaciones):
+            if i == j:
+                continue
+            esEscencial &= ~representacionOpuesta
+
+        if esEscencial != 0:
+            escenciales.append(pareja)
+        else:
+            primos.append(pareja)
+
+    return (escenciales, primos)
+
+def obtenerTerminoMasRelevante(representaciones):
+    terminoMasUtil = None
+    for pareja, representacion in representaciones:
+        cantNumerosUnos = numBitUno(representacion)
+        if terminoMasUtil == None or cantNumerosUnos >= terminoMasUtil[1]:
+            terminoMasUtil = (pareja, cantNumerosUnos)
+
+    return terminoMasUtil[0]
+
+def quineMcCluskey(bit: int, f, redundante) -> list:
     """
         bit: cantidad de bits que se quiere usar
         f: una funcion que devuelve true si la funcion 
             original es 1 o redundante
+        redundantes: una funcion que devuelve true 
+            es redundante
     """
     numeros = []
     for _ in range(bit + 1):
@@ -117,18 +156,50 @@ def quineMcCluskey(bit: int, f) -> list:
         if not diferencia in parejasFinales:
             parejasFinales[diferencia] = pareja
 
-    return parejasFinales.values()
+    parejasFinales = parejasFinales.values()
+    numeros = []
+    for pareja in parejasFinales:
+        numeros += pareja.numeros
+    numerosFinales = {}
+    for numero in numeros:
+        if not numero in numerosFinales and not redundante(numero):
+            numerosFinales[numero] = numero
+
+    numeros = [*numerosFinales.values()]
+
+    primosRestantes = parejasFinales
+    terminosFinales = []
+
+    while len(numeros) > 0:
+        representacionBinario = [ (pareja, obtenerRepresentacionBinaria(pareja, numeros)) for pareja in primosRestantes ]
+        escenciales, primos = obtenerTerminosEscenciales(representacionBinario)
+        for pareja in escenciales:
+            for numero in pareja.numeros:
+                if numero in numeros:
+                    numeros.remove(numero)
+
+        primosRestantes = primos
+
+        terminosFinales += escenciales
+        if len(escenciales) > 0:
+            continue
+
+        terminoUtil = obtenerTerminoMasRelevante(representacionBinario)
+
+        terminosFinales.append(terminoUtil)
+        primosRestantes.remove(terminoUtil)
+
+        for numero in terminoUtil.numeros:
+            if numero in numeros:
+                numeros.remove(numero)
+
+    return terminosFinales
     
 #                              0,    1,    2,                   5,   6,    7,    8,     9,   10,                        14
 funcionInicial = lambda num: [True, True, True, False, False, True, True, True, True, True, True, False, False, False, True, False][num]
+funcionRedundantes = lambda num: [False, False, False, False, False, False, False, True, False, True, False, False, False, False, False, False][num]
 
-resultado = quineMcCluskey(4, funcionInicial)
+resultado = quineMcCluskey(4, funcionInicial, funcionRedundantes)
 for pareja in resultado:
-    print(f"{pareja.numeros} - {pareja.usado}")
+    print(pareja.numeros)
 
-"""
-for i, nivel in enumerate(resultado):
-    print(f"Nivel {i}")
-    for pareja in nivel:
-        print(f"{pareja.numeros} - {pareja.usado}")
-"""
